be5a48674cda89bff25440759bd5db5c6ee395fc
Kaar98 <kanakdwivedi286@gmail.com>
2023-02-07 18:37:01 +0530

    u3

--- src/components/closeFriend/CloseFriend.jsx
+++ src/components/closeFriend/CloseFriend.jsx
@@ -5,7 +5,7 @@ export default function CloseFriend({user}) {
   const PF=process.env.REACT_APP_PUBLIC_FOLDER;
   return (
     <li className="sidebarFriend">
-      <Link to={`https://backend-gwa2.onrender.com/profile/${user._id}`}><img className="sidebarFriendImg"crossorigin="anonymous" src={user.profilePicture||"https://backend-gwa2.onrender.com/images/"+"download.png"} alt="" />
+      <Link to={`/profile/${user._id}`}><img className="sidebarFriendImg"crossorigin="anonymous" src={user.profilePicture||"https://backend-gwa2.onrender.com/images/"+"download.png"} alt="" />
       </Link>
       <span className="sidebarFriendName">{user.userName}</span>
     </li>
